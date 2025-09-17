396a61c6ec45ff75d73c0c6ba7897b5ad6408b65
Lucas Davi Ribeiro <lucasdaviribeiro46@gmail.com>
2025-09-17 12:43:58 -0300

    Update Cabecalho_Charactere_Oriented.py

--- Cabecalho_Charactere_Oriented.py
+++ Cabecalho_Charactere_Oriented.py
@@ -5,26 +5,56 @@ class Cabecalho_Charactere_Oriented:
     FLAG_INICIO = "STX"
     FLAG_FINAL = "ETX"
     FLAG_INTERNA = "DLE"
-    SPACE = " "
+    SPACE = ' '
+    COMMA = ','
 
     #Atributos
     user_data = []
-    transcricao = []
+    transcricao = ''
     indices_Flags = []
 
-    def __init__(self):
-        pass
+    def __init__(self, user_data):
+        self.user_data = user_data
+        
+        self.character_stuffing(self.user_data)
 
     def montar_cabecalho(self):
-        pass
+        self.transcricao = self.FLAG_INICIO
+        self.transcricao += self.SPACE
+        self.transcricao += self.user_data
+        self.transcricao += self.SPACE
+        self.transcricao += self.FLAG_FINAL
+        
 
     def desmontar_cabecalho(self):
         pass
+    
+    # TODO Fazer o Character_Stuffing
+    def character_stuffing(self, user_data):
+        string = self.converte_lista_em_string(user_data)
+        if self.FLAG_INTERNA in string:
+            print(string.split(self.SPACE))
+        elif self.FLAG_FINAL in string:
+            print(string.split(self.FLAG_FINAL))
+        elif self.FLAG_INICIO in string:
+            print(string.split(self.FLAG_INICIO))
+        else:
+            print("Não possui uma flag!\n")
 
+    def to_string(self):
+        return self.transcricao
+
+    def converte_lista_em_string(self, lista):
+        string = ''
+        for valor in lista:
+            string += str(valor)
+
+        return string
 
 if __name__ == "__main__":
     
-    user_data = input("Entrada de Dados: ")
+    user_data = input("Entrada de Dados: ").upper()
+    cabecalho = Cabecalho_Charactere_Oriented(user_data)
 
-    print(f"Entrada: {user_data}")
-
+    cabecalho.montar_cabecalho()
+    print(f"Transcrição: {cabecalho.to_string()}")
